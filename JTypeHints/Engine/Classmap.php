59a86e2ad1a36a9b3db2a9530bbd62edb7952586
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-08-02 17:13:49 +0300

    Move things around

--- JTypeHints/Engine/Classmap.php
+++ JTypeHints/Engine/Classmap.php
@@ -5,7 +5,7 @@
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Akeeba\JTypeHints;
+namespace Akeeba\JTypeHints\Engine;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;

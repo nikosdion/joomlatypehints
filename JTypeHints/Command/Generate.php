0e651787861c5f1dbf2acc865e6bd55a4f65d44d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-08-03 15:18:52 +0300

    Refactor the generate command

---
+++ JTypeHints/Command/Generate.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package   JTypeHints
+ * @copyright Copyright (c) 2017 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Akeeba\JTypeHints\Command;
+
+use Akeeba\JTypeHints\Engine\Classmap;
+use Akeeba\JTypeHints\Engine\Generator;
+use RuntimeException;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Generate
+{
+	public function __invoke($folder, $forVersion, $forSite, OutputInterface $output)
+	{
+		if (empty($folder))
+		{
+			$folder = __DIR__ . '/generated_hints';
+		}
+
+		$parser = null;
+
+		if (!empty($forVersion))
+		{
+			$output->writeln("Generating typehints for Joomla! $forVersion");
+			$forSite = '';
+
+			$parser = Classmap::getForVersion($forVersion);
+		}
+		elseif (!empty($forSite) && is_dir($forSite))
+		{
+			$output->writeln("Generating typehints for Joomla! installation in $forSite");
+			$forVersion = '';
+
+			$parser = Classmap::getForPath($forSite);
+		}
+
+		if (empty($parser))
+		{
+			throw new RuntimeException("A valid Joomla! version or site path is required to run 'generate'");
+		}
+
+		$output->writeln("Typehint classes will be written to $folder");
+
+		$generator = new Generator($parser);
+		$generator->setGeneratedFor(empty($forSite) ? "version $forVersion" : $forSite);
+		$generator->generate($folder);
+	}
+}
--- typehints.php
+++ typehints.php
@@ -5,9 +5,17 @@
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-use Akeeba\JTypeHints\Engine\Classmap;
-use Akeeba\JTypeHints\Engine\Generator;
-use Symfony\Component\Console\Output\OutputInterface;
+use Akeeba\JTypeHints\Command\Generate;
+
+// Has the user run composer install already?
+if (!is_file(__DIR__ . '/vendor/autoload.php'))
+{
+	echo "\n\n\n";
+	echo "* * *  E R R O R  * * *\n\n";
+	echo "Please run composer install before running this application for the first time.\n";
+	echo "If unsure, please take a look at the README.md file. Thank you!\n";
+	echo "\n\n\n";
+}
 
 // Load Composer's autoloader
 $loader = require_once __DIR__ . '/vendor/autoload.php';
@@ -17,49 +25,15 @@ require_once 'version.php';
 
 $app = new Silly\Application('TypeHint Helper for Joomla!', JTHH_VERSION);
 
-$app->command('generate [folder] [--for-version=] [--for-site=]', function ($folder, $forVersion, $forSite, OutputInterface $output) {
-	if (empty($folder))
-	{
-		$folder = __DIR__ . '/generated_hints';
-	}
-
-	$parser = null;
-
-	if (!empty($forVersion)
-		&& version_compare($forVersion, '3.3.0', 'ge')
-		&& version_compare($forVersion, '4.0.0', 'lt'))
-	{
-		$output->writeln("Generating typehints for Joomla! $forVersion");
-		$forSite = '';
-
-		$parser = Classmap::getForVersion($forVersion);
-	}
-	elseif (!empty($forSite) && is_dir($forSite))
-	{
-		$output->writeln("Generating typehints for Joomla! installation in $forSite");
-		$forVersion = '';
-
-		$parser = Classmap::getForPath($forSite);
-	}
-
-	if (empty($parser))
-	{
-		throw new RuntimeException("A valid Joomla! version or site path is required to run 'generate'");
-	}
-
-	$output->writeln("Typehint classes will be written to $folder");
-
-	$generator = new Generator($parser);
-	$generator->setGeneratedFor(empty($forSite) ? "version $forVersion" : $forSite);
-	$generator->generate($folder);
-})->descriptions(
-	'Generates type hints for a specific Joomla! version or installed site',
-	[
-		'folder' => 'Where do you want the typehint class files to be stored. Default: generated_hints',
-		'--for-version' => 'Joomla! version number for which to generate the typehints',
-		'--for-site' => 'Path to a Joomla! installation for which to generate the typehints'
-	]
-);
-//$app->setDefaultCommand('generate');
+$app
+	->command('generate [folder] [--for-version=] [--for-site=]', new Generate())
+	->descriptions(
+		'Generates type hints for a specific Joomla! version or installed site',
+		[
+			'folder'        => 'Where do you want the typehint class files to be stored. Default: generated_hints',
+			'--for-version' => 'Joomla! version number for which to generate the typehints',
+			'--for-site'    => 'Path to a Joomla! installation for which to generate the typehints',
+		]
+	);
 
 $app->run();

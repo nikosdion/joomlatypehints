0dbe3cd10ade0121520047fb3a54c05e464fbf84
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-01-04 23:00:15 +0200

    Update copyright

--- typehints.php
+++ typehints.php
@@ -2,7 +2,7 @@
 <?php
 /**
  * @package   JTypeHints
- * @copyright Copyright (c) 2017-2022 Nicholas K. Dionysopoulos
+ * @copyright Copyright (c) 2017-2023 Nicholas K. Dionysopoulos
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  */
 
